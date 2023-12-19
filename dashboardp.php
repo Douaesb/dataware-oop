022b826cba6d5504a9547d7fb9a6aab47fad38a4
Douae sb <douae.sb411@example.com>
2023-12-19 22:57:06 +0100

    v1

--- dashboardp.php
+++ dashboardp.php
@@ -11,7 +11,7 @@ $productOwner = new ProductOwner($conn);
 $project = new Projet($conn);
 $projects = $project->displayProjects();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submitadd'])) {
     $name = $_POST['nom_pro'];
     $descrp = $_POST['descrp_pro'];
     $productOwner->addProject($name, $descrp);
@@ -84,7 +84,7 @@ if (isset($_GET['id_pro'])) {
                             <div class="">
 
 
-                                <form action="editproject.php" method="post">
+                                <form action="" method="post">
                                     <div>
                                         <label for="" class="block text-sm font-medium leading-6 text-gray-900">Nom projet</label>
                                         <div class="relative mt-2 rounded-md">
@@ -97,7 +97,7 @@ if (isset($_GET['id_pro'])) {
                                             <textarea name="descrp_pro" id="" class="block rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                                         </div>
                                     </div>
-                                    <button type="submit" name="submit" class=" mt-4 ml-14 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Ajouter</button>
+                                    <button type="submit" name="submitadd" class=" mt-4 ml-14 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Ajouter</button>
 
                                 </form>
                             </div>
